254ea46416924210b27df081233bfb4baebacfc0
DMu1e <daltonmulem@gmail.com>
2025-07-20 14:29:24 +0300

    Add basic arithmetic Java classes

---
+++ ArithmeticExeptions.java
@@ -0,0 +1,10 @@
+import java.io.*;
+public class ArithmeticExeptions {
+
+    public static void main(String[] args) {
+        int number1 = 100;
+        int number2 = 100;
+        int answer = number1/number2;
+        System.out.println("Answer"+answer);
+    }
+}
---
+++ ArithmeticJava.java
@@ -0,0 +1,10 @@
+public class ArithmeticJava {
+   public static void main(String[] args) {
+    int number1 = 300;
+    int number2 = 0;
+
+    try{
+        
+    }
+   }  
+}
